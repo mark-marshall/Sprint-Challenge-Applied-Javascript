dfebca013d8270250ba38a44d69ee4d6313c6408
mark-marshall <mark.marshallgp@gmail.com>
2019-02-01 10:23:23 +0000

    [STRETCH] Refactored Carousel class

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -5,48 +5,36 @@
  this.leftButton = document.querySelector('.left-button');
  this.rightButton = document.querySelector('.right-button')
 
- this.activeImage = document.querySelector('.image[data-tab="0"]');
- this.activeImage.style.display = 'block';
  this.currentIndex = 0;
 
  this.leftButton.addEventListener('click', () => {this.decreaseIndexLeft()})
  this.rightButton.addEventListener('click', () =>  {this.increaseIndexRight()})
  }
-
-  
  decreaseIndexLeft() {
-//console.log(this.currentIndex);
  if (this.currentIndex === 0) {
     this.currentIndex = 3;
  }  
  else {this.currentIndex = this.currentIndex-1};
 
- //console.log(this.currentIndex);
-
 this.allImages = document.querySelectorAll('.image');
 this.allImages.forEach(img => img.style.display='none');
 
 this.activeImage = document.querySelector(`.image[data-tab="${this.currentIndex}"]`);
-this.activeImage.style.display ='block';
+this.activeImage.style.display='block';
 
  }
  increaseIndexRight() {
-//console.log(this.currentIndex);
  if (this.currentIndex === 3) {
      this.currentIndex = 0;
  }
  else {this.currentIndex = this.currentIndex + 1}
-
- //console.log(this.currentIndex);
  
  this.allImages = document.querySelectorAll('.image');
  this.allImages.forEach(img => img.style.display='none');
 
  this.activeImage = document.querySelector(`.image[data-tab="${this.currentIndex}"]`);
  this.activeImage.style.display ='block';
-
  }
- 
 }
 
 let carousel = document.querySelectorAll('.carousel').forEach(car => new Carousel(car));
