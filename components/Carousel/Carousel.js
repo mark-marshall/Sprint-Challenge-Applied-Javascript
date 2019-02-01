8a38e121c7a72f14533206f454a6948095c2f94d
mark-marshall <mark.marshallgp@gmail.com>
2019-02-01 09:18:06 +0000

    [STRETCH] First draft non-working carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,48 @@
 class Carousel {
+ constructor(carouselElement) {
+ this.carousel = carouselElement;
 
+ this.leftButton = document.querySelector('.left-button');
+ this.rightButton = document.querySelector('.right-button')
+
+ currentIndex = 0;
+ this.firstImage = document.querySelector(`.image[data-tab"0"]`);
+ this.firstImage.style.display = 'block';
+
+ this.leftButton.addEventListener('click', () => {this.decreaseIndexLeft()})
+ this.rightButton.addEventListener('click', () =>  {this.increaseIndexRight()})
+ }
+
+ dereaseIndexLeft() {
+ if (currentIndex = 0) {
+    currentIndex = 3
+ }  
+ else {currentIndex = currentIndex-1}
+
+ this.allImages = document.querySelectorAll('.image');
+ this.allImages.forEach(img => img.style.display='none');
+
+ this.activeImage = document.querySelector(`.image[data-tab"${currentIndex}"]`);
+ this.activeImage.style.display ='block';
+
+ }
+ increaseIndexRight() {
+ if (currentIndex = 3) {
+     currentIndex = 0
+ }
+ else {currentIndex = currentIndex+1}
+ 
+ this.allImages = document.querySelectorAll('.image');
+ this.allImages.forEach(img => img.style.display='none');
+
+ this.activeImage = document.querySelector(`.image[data-tab"${currentIndex}"]`);
+ this.activeImage.style.display ='block';
+
+ }
 }
 
-let carousel = document.querySelector();
+
+let carousel = document.querySelector('.carousel');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
